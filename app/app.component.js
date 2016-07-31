eb81dfae1ce7cd949024aef73c80877ea41c021f
emily.c.ritter <emily.c.ritter@gmail.com>
2016-07-31 18:18:48 -0500

    Codeschool 1.1

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Angular 2 Intro';
+        this.firstObject = {
+            "id": 1,
+            "name": 'My first Object',
+            "description": "This is my first object that I will send in.",
+            "quantity": 1
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: "<h1>{{title}}</h1>\n    <h2>{{firstObject.name}}</h2>\n    <p>{{firstObject.description}}</p>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `<h1>{{title}}</h1>
+    <h2>{{firstObject.name}}</h2>
+    <p>{{firstObject.description}}</p>`
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Angular 2 Intro',
+  firstObject = {
+    "id": 1,
+    "name": 'My first Object',
+    "description": "This is my first object that I will send in."
+    "quantity": 1
+  }
+}
